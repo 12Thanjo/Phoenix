4480d5c9e1ac8106905f5ba46b41eeb198ba0e30
12Thanjo <alfeldman12@gmail.com>
2022-06-19 20:07:22 -0400

    fixed jx premake

--- Phoenix/lib/jx/premake5.lua
+++ Phoenix/lib/jx/premake5.lua
@@ -7,32 +7,5 @@ project "jx"
 	-- cppdialect "C++20"
 	staticruntime "on"
 	
-	targetdir ("bin/bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin/intermediates/" .. outputdir .. "/%{prj.name}")
-
-	files{
-		"**.h",
-		"**.cpp",
-		-- "jxcore.cpp"
-	}
-
-
-	libdirs {
-		"./x64"
-	}
-
-	defines{
-		"JS_ENGINE_V8",
-		"JXCORE_EMBEDDED",
-		"WIN64",
-		"NDEBUG",
-		"_CONSOLE",
-		"_LIB",
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-		staticruntime "On"
-
-	filter { "system:windows", "configurations:Release"}
-		buildoptions "/MT"
+	targetdir ("x64")
+	objdir ("bin-int")
